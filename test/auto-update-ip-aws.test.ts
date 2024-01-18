55da5a6e04f50bc7a845c09ed4bf4a629ad9cb20
guiyom-e <guillaume.egee@outlook.com>
2024-01-18 11:21:02 +0100

    Initial commit

---
+++ test/auto-update-ip-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AutoUpdateIpAws from '../lib/auto-update-ip-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/auto-update-ip-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AutoUpdateIpAws.AutoUpdateIpAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
